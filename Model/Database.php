aba4f5c9ca21c158fbc0a0fdac3b4c42d9bb06f7
Jonatan Ismael <jonatan@almbrasil.com>
2024-04-20 02:46:11 -0300

    First Commit

---
+++ Model/Database.php
@@ -0,0 +1,20 @@
+<?php 
+
+class Database
+{
+    public function select(int $limit) : array
+    {
+        try {
+            $users = json_decode(file_get_contents(DATABASE_FILE), true);
+            $users = array_slice($users, 0, $limit);
+            return $users;
+            
+        }catch(Exception $e){
+            throw New Exception($e->getMessage());
+        }
+
+        return false;
+    }
+}
+
+?>
---
+++ Model/UserModel.php
@@ -0,0 +1,12 @@
+<?php 
+
+require_once ROOT_PATH . "/Model/Database.php";
+class UserModel extends Database
+{
+    public function getUsers(int $limit)
+    {
+        return $this->select($limit);
+    }
+}
+
+?>
